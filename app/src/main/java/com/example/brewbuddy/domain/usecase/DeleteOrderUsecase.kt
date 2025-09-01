654c118795cd5fb34b2fa6683483dbd822d77967
Eman <aymanmjdyabwjbl@gmail.com>
2025-09-01 15:56:08 +0300

    changed files

--- app/src/main/java/com/example/brewbuddy/domain/usecase/DeleteOrderUsecase.kt
+++ app/src/main/java/com/example/brewbuddy/domain/usecase/DeleteOrderUsecase.kt
@@ -1,11 +1,11 @@
 package com.example.brewbuddy.domain.usecase
 
-import com.example.brewbuddy.domain.model.Order
+import com.example.brewbuddy.data.orderdb.OrderEntity
 import com.example.brewbuddy.domain.repository.OrderRepository
 import javax.inject.Inject
 
 class DeleteOrderUseCase @Inject constructor(
     private val repository: OrderRepository
 ) {
-    suspend operator fun invoke(order: Order) = repository.deleteOrder(order)
+    suspend operator fun invoke(order: OrderEntity) = repository.deleteOrder(order)
 }
